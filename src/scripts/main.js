98caa1be4b3789477108dc23d2f52a23b7a77ddd
JulianeMonteiro <juliane.a.monteiro@hotmail.com>
2023-07-13 00:34:36 -0300

    Criando um contador para o meu aniversário

---
+++ src/scripts/main.js
@@ -0,0 +1,30 @@
+const dataEvento = new Date("Nov 16, 2023 19:00:00");
+const timeStampEvento = dataEvento.getTime();
+
+const contaHoras = setInterval(function () {
+  const agora = new Date();
+  const timeStampAtual = agora.getTime();
+
+  const intervaloAteEvento = timeStampEvento - timeStampAtual;
+
+  const diaEmMs = 1000 * 60 * 60 * 24;
+  const horaEmMs = 1000 * 60 * 60;
+  const minutoEmMs = 1000 * 60;
+
+  const diasAteEvento = Math.floor(intervaloAteEvento / diaEmMs);
+  const horasAteEvento = Math.floor((intervaloAteEvento % diaEmMs) / horaEmMs);
+  const minutosAteEvento = Math.floor(
+    (intervaloAteEvento % horaEmMs) / minutoEmMs
+  );
+  const segundoAteEvento = Math.floor((intervaloAteEvento % minutoEmMs) / 1000);
+
+  document.getElementById(
+    "contador"
+  ).innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundoAteEvento}s`;
+
+  if (intervaloAteEvento < 0) {
+    clearInterval(contaHoras);
+
+    document.getElementById("contador").innerHTML = `Evento expirado`;
+  }
+}, 1000);
